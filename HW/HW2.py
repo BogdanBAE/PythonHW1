bed2f2dd9191991f22f11dcf5524a1592112f2e5
BogdanBAE <bogdan.elistratov@gmail.com>
2022-11-25 14:08:06 +0400

    Task 2 Done2

--- HW/HW2.py
+++ HW/HW2.py
@@ -2,7 +2,7 @@ x = int(input())
 y = int(input())
 z = int(input())
 
-if ((not (x or y or z)) == ((not x) or (not y) or (not y))):
+if ((not (x or y or z)) == ((not x) and (not y) and (not y))):
     print('True')
 else:
     print('False')
